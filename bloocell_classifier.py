37d179db00067b63f93c1a8ae8dd659bd9bdfb90
ulisrael <ulisrael@gmail.com>
2024-06-20 10:48:10 -0700

    added dropout, data aug, and weight decay

--- bloocell_classifier.py
+++ bloocell_classifier.py
@@ -43,16 +43,20 @@ class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.dropout1 = nn.Dropout(0.2)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.dropout2 = nn.Dropout(0.2)
         self.fc1 = nn.Linear(64 * (IMG_HEIGHT // 4) * (IMG_WIDTH // 4), 128)
         self.fc2 = nn.Linear(128, 2)  # Binary classification
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
         x = self.fc2(x)
         return x
 
@@ -95,14 +99,23 @@ if __name__ == "__main__":
     val_csv_file = args.val_csv_file
 
     # Define transformations
-    transform = transforms.Compose([
+    train_transform = transforms.Compose([
+        # transforms.Resize((IMG_HEIGHT, IMG_WIDTH), antialias=True),
+        transforms.RandomResizedCrop((IMG_HEIGHT, IMG_WIDTH), antialias=True),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    val_transform = transforms.Compose([
         transforms.Resize((IMG_HEIGHT, IMG_WIDTH), antialias=True),
         # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     # Load the datasets
-    train_dataset = CustomImageDataset(csv_file=train_csv_file, img_dir=train_img_dir, transform=transform)
-    val_dataset = CustomImageDataset(csv_file=val_csv_file, img_dir=val_img_dir, transform=transform)
+    train_dataset = CustomImageDataset(csv_file=train_csv_file, img_dir=train_img_dir, transform=train_transform)
+    val_dataset = CustomImageDataset(csv_file=val_csv_file, img_dir=val_img_dir, transform=val_transform)
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     val_loader = DataLoader(dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=False)
@@ -111,7 +124,7 @@ if __name__ == "__main__":
     # Instantiate the model, loss function, and optimizer
     model = SimpleCNN().to(device)  # Move the model to the GPU
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-3)
 
     # Training loop
     for epoch in range(EPOCHS):
